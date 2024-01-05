6d54e95de8ba550a383f9c5429916d20b58738c6
FiSpro <filipspro@gmail.com>
2024-01-05 02:26:53 +0100

    Added controller and endpoints

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore() 
+        {
+            // init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York",
+                    Description = "The one with the big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Ohrid",
+                    Description = "The one with the lake."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower."
+                }
+            };
+        }
+    }
+}
